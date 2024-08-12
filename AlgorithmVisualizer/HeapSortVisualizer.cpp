48e0f2ab130d4face051cdb8a1e11b7c4edc92e3
Yanko Mirov <yanicha93@gmail.com>
2024-08-12 13:37:21 +0300

    Refactor logic

--- AlgorithmVisualizer/HeapSortVisualizer.cpp
+++ AlgorithmVisualizer/HeapSortVisualizer.cpp
@@ -17,37 +17,18 @@ void HeapSortVisualizer::step() {
 void HeapSortVisualizer::draw(sf::RenderWindow& window) {
 	float barWidth = static_cast<float>(window.getSize().x) / initialSize;
 
-	if (pq.empty()) {
-		for (int i = 0; i < sortedItems.size(); i++) {
-			sf::RectangleShape bar(sf::Vector2f(barWidth - 1, sortedItems[i]));
-			bar.setPosition(i++ * barWidth, window.getSize().y - sortedItems[i]);
-			bar.setFillColor(sf::Color::Green);  // Different color for sorted elements
-			window.draw(bar);
-
-			//drawBar(i, barWidth - 1, sortedItems[i], sf::Color::Green, window);
-		}
-	}
-
 	int currentElementIndex = 0;
 
 	// Draw the sorted elements
 	for (int elem : sortedItems) {
-		sf::RectangleShape bar(sf::Vector2f(barWidth - 1, elem));
-		bar.setPosition(currentElementIndex++ * barWidth, window.getSize().y - elem);
-		bar.setFillColor(sf::Color::Green);  // Different color for sorted elements
-		window.draw(bar);
-		//drawBar(currentElementIndex++, barWidth, window.getSize().y - elem, sf::Color::Green, window);
+		drawBar(currentElementIndex++, barWidth, elem, sf::Color::Green, window);
 	}
 
 	// Shuffle remaining elements to show they are not sorted yet
 	std::shuffle(shuffledItems.begin() + (sortedItems.size() - 1), shuffledItems.end(), std::mt19937{ std::random_device{}() });
 
 	for (int elem : shuffledItems) {
-		sf::RectangleShape bar(sf::Vector2f(barWidth - 1, elem));
-		bar.setPosition(currentElementIndex++ * barWidth, window.getSize().y - elem);
-		bar.setFillColor(sf::Color::White);  // Color for unsorted elements
-		window.draw(bar);
-		//drawBar(currentElementIndex++, barWidth, window.getSize().y - elem, sf::Color::White, window);
+		drawBar(currentElementIndex++, barWidth, elem, sf::Color::Green, window);
 	}
 }
 
